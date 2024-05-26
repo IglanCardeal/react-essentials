0f7c0b8d98275dc66798570b492956aebe4b9748
IglanCardeal <cmtcardeal@outlook.com>
2024-05-26 16:33:04 -0300

    refactor: move Header to a separated component

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import ReactCoreConceptsImg from '../assets/react-core-concepts.png'
+
+export function Header() {
+  return (
+    <header>
+      <img src={ReactCoreConceptsImg} alt='Stylized atom' />
+      <h1>React Essentials</h1>
+      <p>
+        Fundamental React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  )
+}
